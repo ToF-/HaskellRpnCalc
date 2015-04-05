ca1ae1386d1534dd9617e4e4a4755a19bc86dcaa
ToF- <cthibaut@octo.com>
2015-04-05 16:07:49 +0200

    refactoring using >>=

--- RPNCalculator.hs
+++ RPNCalculator.hs
@@ -1,27 +1,30 @@
-module RPNCalculator (calculator, result, (-:), Operation (..))
+module RPNCalculator (Calculator, calculator, pop, push, binary, unary )
 where
+import Control.Monad
 
 type Stack = [Double]
-data Operation = Number Double
-               | Unary (Double -> Double)
-               | Binary (Double -> Double -> Double)
-
 type Calculator = Either String Stack
-
-infixl 5 -:
 
 calculator = Right []
 
 result :: Calculator -> Either String Double 
-result (Left m) = Left m
-result (Right []) = Left "stack is empty - no result"
-result (Right (n:_)) = Right n
+result c = c >>= pop
 
-(-:) :: Calculator -> Operation -> Calculator
-(-:) (Left m) _ = Left m
-(-:) (Right ns) (Number n) = Right (n:ns) 
-(-:) (Right ([])) (Unary _) = Left "not enough parameters - no result" 
-(-:) (Right (n:[])) (Binary _) = Left "not enough parameters - no result" 
-(-:) (Right (n:ns)) (Unary op) = Right (op n:ns)
-(-:) (Right (n:m:ns)) (Binary op) = Right (op n m:ns)
+pop :: Stack -> Either String Double
+pop [] = Left "stack is empty - no result"
+pop (n:_) = Right n
+
+push :: Double -> Stack -> Calculator
+push n ns = Right (n:ns)
+
+binary :: (Double -> Double -> Double) -> Stack -> Calculator
+binary f ns | length ns < 2  = Left "not enough parameters - no result"
+binary (/) (0:_) = Left "division by zero - no result" 
+binary f (n:m:ns) = Right ((f m n):ns)
+
+unary :: (Double -> Double) -> Stack -> Calculator
+unary f [] = Left "not enough parameters - no result" 
+unary f (n:ns) = Right (f n :ns)
+
+
 
--- Specs.hs
+++ Specs.hs
@@ -1,30 +1,38 @@
 import Test.Hspec
 import RPNCalculator
 
+c :: Calculator
 c = calculator
 main = hspec $ do
     describe "RPN calculator" $ do
         it "should not yield a result when not given numbers" $ do
-            result c `shouldBe` Left "stack is empty - no result"
+            (c >>= pop) `shouldBe` Left "stack is empty - no result"
 
         it "should yield the last number stacked as a result" $ do
-            result (c -: Number 4807.00) `shouldBe` Right 4807.00
-            result (c -: Number 1 -: Number 2) `shouldBe` (Right 2.0)
+            (c >>= push 4807.00 >>= pop) `shouldBe` Right 4807.00
+            (c >>= push 1 >>= push 2 >>= pop) `shouldBe` (Right 2.0)
 
         it "should support addition" $ do
-            result (c -: Number 1 -: Number 2 -: Binary (+)) `shouldBe` (Right 3.0)
+            (c >>= push 1 >>= push 2 >>= binary (+) >>= pop) `shouldBe` (Right 3.0)
+
+        it "should support subtraction" $ do
+            (c >>= push 1 >>= push 2 >>= binary (-) >>= pop) `shouldBe` (Right (-1.0))
 
         it "yields an error when not enough numbers on the stack for binary operation" $ do
-            result (c -: Number 1 -: Binary (+)) `shouldBe` (Left "not enough parameters - no result")
+            (c >>= push 1 >>= binary (+) >>= pop) `shouldBe` (Left "not enough parameters - no result")
 
         it "stops at the first error when given not enough numbers" $ do
-            result (c -: Number 1 -: Binary (+) -: Number 2 -: Binary (*)) `shouldBe`(Left "not enough parameters - no result")
+            (c >>= push 1 >>= binary (+) >>= push 2 >>= binary (*) >>= pop) `shouldBe`(Left "not enough parameters - no result")
 
         it "should support unary operation" $ do
-            result (c -: Number 48.05 -: Unary (negate)) `shouldBe` (Right (-48.05))
+            (c >>= push 48.05 >>= unary (negate) >>= pop) `shouldBe` (Right (-48.05))
 
         it "yields an error when not enough numbers for unary operation" $ do
-            result (c -: Unary (negate)) `shouldBe` (Left "not enough parameters - no result")
+            (c >>= unary (negate) >>= pop) `shouldBe` (Left "not enough parameters - no result")
+
+        it "yields an error when trying to divide by 0" $ do
+            (c >>= push 1 >>= push 0 >>= binary (/) >>= pop) `shouldBe` (Left "division by zero - no result")
+
 
  
 
