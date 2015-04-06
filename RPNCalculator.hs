24eb706058d7215a1c0066fa7eca8e77182201fc
ToF- <cthibaut@octo.com>
2015-04-06 12:10:45 +0200

    starting over

--- RPNCalculator.hs
+++ RPNCalculator.hs
@@ -1,49 +1,5 @@
-module RPNCalculator (Calculator, calculator, pop, push, binary, unary, eval, output, process)
+module RPNCalculator (eval)
 where
-import Control.Monad
 
-type Stack = [Double]
-type Calculator = Either String Stack
-type Result = Either String Double
-
-calculator :: Calculator
-calculator = Right []
-
-pop :: Stack -> Result
-pop [] = Left "stack is empty - no result"
-pop (n:_) = Right n
-
-push :: Double -> Stack -> Calculator
-push n ns = Right (n:ns)
-
-binary :: (Double -> Double -> Double) -> Stack -> Calculator
-binary f ns | length ns < 2  = Left "not enough parameters - no result"
-binary (/) (0:_) = Left "division by zero - no result" 
-binary f (n:m:ns) = Right ((f m n):ns)
-
-unary :: (Double -> Double) -> Stack -> Calculator
-unary f [] = Left "not enough parameters - no result" 
-unary f (n:ns) = Right (f n :ns)
-
-message :: String -> Stack -> Calculator
-message s _ = Left s
-
-eval :: String -> Either String Double
-eval ss = (foldM (flip readOper) [] $ words ss) >>= pop
-    where
-    readOper :: String -> Stack -> Calculator
-    readOper "neg" = unary negate
-    readOper "*"   = binary (*)
-    readOper "/"   = binary (/)
-    readOper "+"   = binary (+)
-    readOper "-"   = binary (-)
-    readOper op    = case reads op of
-                        [(d,_)] -> push d
-                        []      -> message (op ++ " ? - no result") 
-
-output :: Either String Double -> String
-output (Right n) = show n
-output (Left s)  = s
-
-process :: String -> String
-process = unlines . map (output . eval) . lines
+eval :: String -> String
+eval = id
--- Specs.hs
+++ Specs.hs
@@ -1,57 +1,7 @@
 import Test.Hspec
 import RPNCalculator
 
-c :: Calculator
-c = calculator
 main = hspec $ do
     describe "RPN calculator" $ do
-        it "should not yield a result when not given numbers" $ do
-            (c >>= pop) `shouldBe` Left "stack is empty - no result"
-
-        it "should yield the last number stacked as a result" $ do
-            (c >>= push 4807.00 >>= pop) `shouldBe` Right 4807.00
-            (c >>= push 1 >>= push 2 >>= pop) `shouldBe` (Right 2.0)
-
-        it "should support addition" $ do
-            (c >>= push 1 >>= push 2 >>= binary (+) >>= pop) `shouldBe` (Right 3.0)
-
-        it "should support subtraction" $ do
-            (c >>= push 1 >>= push 2 >>= binary (-) >>= pop) `shouldBe` (Right (-1.0))
-
-        it "yields an error when not enough numbers on the stack for binary operation" $ do
-            (c >>= push 1 >>= binary (+) >>= pop) `shouldBe` (Left "not enough parameters - no result")
-
-        it "stops at the first error when given not enough numbers" $ do
-            (c >>= push 1 >>= binary (+) >>= push 2 >>= binary (*) >>= pop) `shouldBe`(Left "not enough parameters - no result")
-
-        it "should support unary operation" $ do
-            (c >>= push 48.05 >>= unary (negate) >>= pop) `shouldBe` (Right (-48.05))
-
-        it "yields an error when not enough numbers for unary operation" $ do
-            (c >>= unary (negate) >>= pop) `shouldBe` (Left "not enough parameters - no result")
-
-        it "yields an error when trying to divide by 0" $ do
-            (c >>= push 1 >>= push 0 >>= binary (/) >>= pop) `shouldBe` (Left "division by zero - no result")
-
-        it "converts a string into RPN instructions" $ do
-            eval "48.07"     `shouldBe` (Right 48.07)
-            eval "1 2 3"     `shouldBe` (Right  3)
-            eval "2 5 *"     `shouldBe` (Right 10) 
-            eval "3 neg"     `shouldBe` (Right (-3)) 
-            eval "1 2 +"     `shouldBe` (Right  3)
-            eval "1 2 /"     `shouldBe` (Right 0.5)
-            eval "1 3 - neg" `shouldBe` (Right  2)
-
-        it "outputs correct result" $ do
-            output (Right 48.07) `shouldBe` "48.07"
-            output (Left "foo") `shouldBe` "foo"
-
-        it "gracefully process bad entry" $ do
-            eval "foo" `shouldBe` (Left "foo ? - no result")
-
-        it "processes all lines in input" $ do
-            process "1 2 +\n4 2 -\n" `shouldBe` "3.0\n2.0\n"
-
- 
-
-
+        it "should read a correct number" $ do
+            eval "4807" `shouldBe` "4807"
