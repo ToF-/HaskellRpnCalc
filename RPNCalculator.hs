adc2dd238e419f081ce52c1b9f605e63eceaf9d7
ToF- <cthibaut@octo.com>
2015-04-06 14:29:23 +0200

    refactored to Stack

--- RPNCalculator.hs
+++ RPNCalculator.hs
@@ -1,14 +1,16 @@
 module RPNCalculator (eval)
 where
-type Stack = String
+type Stack = Either String [Int]
 
 eval :: String -> String
 eval "7000 84  +" = "7084"
-eval s = foldl evalWord "" $ words s
+eval s = case foldl evalWord (Right []) $ words s of
+            Right (n:ns) -> show n
+            Left m       -> m
 
 evalWord :: Stack -> String -> Stack
-evalWord stack "neg" = "-" ++ stack 
-evalWord stack "+"   = "4807"
-evalWord stack s = case reads s :: [(Int,String)] of
-                [(n,_)] -> show n
-                []      -> s ++ " ? - no result"
+evalWord (Right (n:ns))  "neg" = Right (-n:ns)  
+evalWord (Right (n:m:ns)) "+"  = Right (n+m:ns)
+evalWord (Right ns) s = case reads s :: [(Int,String)] of
+                [(n,_)] -> Right (n:ns)
+                []      -> Left (s ++ " ? - no result")
