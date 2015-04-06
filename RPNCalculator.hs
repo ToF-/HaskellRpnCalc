40591189d2a10a90c417b5fa3f640565b6968434
ToF- <cthibaut@octo.com>
2015-04-06 14:40:42 +0200

    extract binary

--- RPNCalculator.hs
+++ RPNCalculator.hs
@@ -17,6 +17,9 @@ push (Right ns) n = Right (n:ns)
 
 oper :: Stack -> String -> Stack
 oper (Right (n:ns))  "neg" = Right (-n:ns)  
-oper (Right (n:m:ns)) "+"  = Right (n+m:ns)
+oper st "+"  = binary (+) st
 oper (Right (n:m:ns)) "*"  = Right (n*m:ns)
 oper (Right _) s = Left (s ++ " ? - no result")
+
+binary :: (Int -> Int -> Int) -> Stack -> Stack
+binary f (Right (n:m:ns)) = Right (f n m:ns)
