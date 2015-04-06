ce1dfb3dc5469bd8c026f871a22d2098d3ac44c5
ToF- <cthibaut@octo.com>
2015-04-06 12:17:34 +0200

    generalize

--- RPNCalculator.hs
+++ RPNCalculator.hs
@@ -2,7 +2,6 @@ module RPNCalculator (eval)
 where
 
 eval :: String -> String
-eval "foo" = "foo ? - no result"
 eval s = case reads s :: [(Int,String)] of
             [(n,_)] -> show n
             []      -> s ++ " ? - no result"
--- Specs.hs
+++ Specs.hs
@@ -5,6 +5,8 @@ main = hspec $ do
     describe "RPN calculator" $ do
         it "should read a correct number" $ do
             eval "4807" `shouldBe` "4807"
+            eval "7084" `shouldBe` "7084"
 
         it "should signal an incorrect entry" $ do
             eval "foo" `shouldBe` "foo ? - no result"
+            eval "bar" `shouldBe` "bar ? - no result"
