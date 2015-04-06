5aeb71ded0cc5a8777bb3eaea06b6ae025c1c1d5
ToF- <cthibaut@octo.com>
2015-04-06 12:15:17 +0200

    reads a correct entry - signals error

--- RPNCalculator.hs
+++ RPNCalculator.hs
@@ -2,4 +2,7 @@ module RPNCalculator (eval)
 where
 
 eval :: String -> String
-eval = id
+eval "foo" = "foo ? - no result"
+eval s = case reads s :: [(Int,String)] of
+            [(n,_)] -> show n
+            []      -> s ++ " ? - no result"
--- Specs.hs
+++ Specs.hs
@@ -5,3 +5,6 @@ main = hspec $ do
     describe "RPN calculator" $ do
         it "should read a correct number" $ do
             eval "4807" `shouldBe` "4807"
+
+        it "should signal an incorrect entry" $ do
+            eval "foo" `shouldBe` "foo ? - no result"
