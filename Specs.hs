150b81f6923c6574de2e8173d51eebe0481bb914
ToF- <cthibaut@octo.com>
2015-04-07 08:57:45 +0200

    chaining pushes

--- Specs.hs
+++ Specs.hs
@@ -5,3 +5,7 @@ main = hspec $ do
     describe "RPN Calculator" $ do
         it "should push numbers on a stack" $ do
             push 4807 [] `shouldBe` Right [4807]
+            (Right [] >>=
+             push 1   >>=
+             push 2   >>= 
+             push 3) `shouldBe` Right [3,2,1]
