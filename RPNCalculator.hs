2b0f181632567ab10660a375fbb0ad2878d073b0
ToF- <cthibaut@octo.com>
2015-04-07 10:43:31 +0200

    refactoring to inspect propagation

--- RPNCalculator.hs
+++ RPNCalculator.hs
@@ -25,9 +25,7 @@ inspect _ _ = Left "not enough parameters - no result"
 
 safeUnary :: (Int -> Int) -> Stack -> Calculator
 safeUnary f (n:ns) = Right (f n:ns)
-safeUnary _ _ = Left "not enough parameters - no result"
 
 safeBinary :: (Int -> Int -> Int) -> Stack -> Calculator
 safeBinary f (n:m:ns) = Right (f m n:ns)
-safeBinary _ _ = Left "not enough parameters - no result"
 
