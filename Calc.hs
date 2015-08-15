fba56926d50b7f80410c68b046909ef90e91032d
ToF- <cthibaut@octo.com>
2015-08-15 11:23:20 +0200

    refactoring: replace eval with (>>=)

--- Calc.hs
+++ Calc.hs
@@ -6,13 +6,10 @@ type Number = Int
 type Message = String
 
 calc :: String -> String
-calc = result . foldl eval initial . map parse . words
+calc = result . foldl (>>=) initial . map parse . words
     where initial = Right 0
           result (Right n) = show n
           result (Left m)  = m
-
-eval :: Calc -> (Number -> Calc) -> Calc
-eval c f = c >>= f 
 
 parse :: String -> Number -> Calc
 parse s = case reads s :: [(Number,String)] of
