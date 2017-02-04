2490526033cbd9673cfc021e55a47812bd34ad77
ToF <cthibaut@octo.com>
2017-02-04 10:56:36 +0100

    much much simpler version with foldM

--- RpnCalc.hs
+++ RpnCalc.hs
@@ -6,23 +6,21 @@ type Calc = Either Message Stack
 type Stack = [Int]
 type Message = String
 
-calc =  result . foldM eval [] . words
+calc =  result . foldM (flip eval) [] . words
     where
     result = either id (show.head)
     
-    eval :: Stack -> String -> Calc
-    eval st "neg" = pop st >>= top negate
-    eval st "abs" = pop st >>= top abs
-    eval st s     = parse s st
+    eval :: String -> Stack -> Calc
+    eval "neg" = unary negate
+    eval "abs" = unary abs
+    eval s     = parse s
+
+    unary :: (Int -> Int) -> Stack -> Calc
+    unary f []     = Left "missing parameter"
+    unary f (n:ns) = Right (f n:ns)
      
     parse :: String -> Stack -> Calc
     parse s ns = case reads s of
         [(n,_)] -> Right (n:ns)
         []      -> Left (s ++ "?")
 
-    top :: (Int -> Int) -> (Int,Stack) -> Calc
-    top f (n,ns) = Right (f n:ns)
-
-    pop :: Stack -> Either Message (Int,Stack)
-    pop (n:ns) = Right (n,ns)
-    pop []     = Left "missing parameter"
