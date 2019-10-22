f2eadf1f132ce8f99d8742fd50dd2e39fca1b37b
ToF- <cthibaut@octo.com>
2019-10-22 05:16:46 +0200

    unary applies a unary operation on the top of the stack

--- RpnCalc.hs
+++ RpnCalc.hs
@@ -3,6 +3,8 @@ where
 
 type Stack = [Integer]
 data Token = Const Integer
+           | Unary (Integer -> Integer)
 
 eval :: Stack -> Token -> Stack
 eval st (Const n) = n : st
+eval (n:ns) (Unary f) = f n : ns
--- Specs.hs
+++ Specs.hs
@@ -3,8 +3,11 @@ import RpnCalc
 
 main = hspec $ do
     describe "eval" $ do
-        describe "can eval tokens using a stack:" $ do
+        describe "can evaluate tokens using a stack:" $ do
             it "const pushes a constant on the stack" $ do
                 [] `eval` (Const 42) `shouldBe` [42]
                 [] `eval` (Const 17) `shouldBe` [17]
                 [42] `eval` (Const 17) `shouldBe` [17,42]
+
+            it "unary applies a unary operation on the top of the stack" $ do 
+                [42] `eval` (Unary negate) `shouldBe` [-42]
