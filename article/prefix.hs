e5912edba2621babca12a45eea0e41003ddfeaeb
ToF- <cthibaut@octo.com>
2019-10-31 09:30:22 +0100

    have two distinct parsers for token and for eval

--- article/prefix.hs
+++ article/prefix.hs
@@ -1,36 +1,67 @@
 import Data.Char
 import Data.List
 
-data Prefix = Number Integer
-            | UnOp Char (Integer -> Integer) Prefix
-            | BinOp Char (Integer -> Integer -> Integer) Prefix Prefix
+type Number = Integer
 
-instance Show Prefix where
-    show (Number n) = show n
-    show (UnOp c f p) = "(" ++ [c] ++ " " ++ (show p)++")"
-    show (BinOp c f p q) = "(" ++ [c] ++ " " ++ (show p) ++ " " ++ (show q)++")"
+data Token = Num Number
+           | Op1 Char (Number -> Number)
+           | Op2 Char (Number -> Number -> Number)
 
-type Parser = String -> [(Prefix ,String)]
+instance Show Token
+    where
+    show (Num n) = "Num " ++ show n
+    show (Op1 c f) = "Op1 " ++ [c] 
+    show (Op2 c f) = "Op2 " ++ [c] 
 
-prefix :: Parser 
-prefix (' ':s) = prefix s
-prefix (c:s) = case c `lookup` unOps of
-    Just f -> let [(p,s')] = prefix s in [(UnOp c f p,s')]
-    Nothing -> case c `lookup` binOps of
-        Just f -> let
-                    [(p,s')] = prefix s
-                    [(q,s'')] = prefix s'
-               in [(BinOp c f p q,s'')]
-        Nothing -> number (c:s)
-prefix _ = []
-    
+type Parser a b = [a] -> [(b,[a])]
 
-number :: Parser
-number s = case reads s :: [(Integer,String)] of
+digit :: Parser Char Number
+digit (c:s) | isDigit c = [(fromIntegral (digitToInt c), s)]
+digit _ = []
+
+accum :: Number -> Parser Char Number
+accum n s = case digit s of
+    [] -> [(n,s)]
+    [(d,s')] -> accum (n * 10 + d) s'
+
+number :: Parser Char Token
+number (c:s) | isSpace c = number s
+number (c:s) | isDigit c = [(Num n,s')] where [(n,s')] = accum 0 (c:s)
+number _ = []
+
+op1 :: Char -> (Number -> Number) -> Parser Char Token
+op1 o f (c:s) | c == o = [(Op1 c f, s)]
+op1 _ _ _ = []
+
+negation = op1 '~' negate
+factorial = op1 '!' (\n -> product [1..n])
+
+infix 2 <|>
+
+(<|>) :: Parser a b -> Parser a b -> Parser a b
+parserA <|> parserB = \s -> case parserA s of
+    [] -> parserB s
+    r  -> r
+
+unary = negation <|> factorial
+
+op2 :: Char -> (Number -> Number -> Number) -> Parser Char Token
+op2 o f (c:s) | c == o = [(Op2 c f, s)]
+op2 _ _ _ = []
+
+binary = foldl1 (<|>) (map (uncurry op2) binOps)
+
+binOps = [('+',(+)),('-',flip (-)),('*',(*)),('/',(flip div)),('%',flip mod)]
+
+infix 3 <&>
+
+(<&>) :: Parser a b -> Parser a b -> Parser a b
+parserA <&> parserB = \s -> case parserA s of
     [] -> []
-    [(n,s)] -> [(Number n,s)]
+    [(a,s')] -> parserB s'
 
-unOps = [('~', negate), ('!', factorial)]
-binOps = [('+', (+)), ('-', flip (-)), ('*', (*)), ('/', flip div), ('%', flip mod)]
-
-factorial n = product [1..n]
+expr :: Parser Char [Token]
+expr s = [([t],s')] 
+    where
+    [(t,s')] = number s 
+expr _ = []
