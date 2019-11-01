72b1df5a400e86b6b2eb12a92365c76bf7b4e740
ToF- <cthibaut@octo.com>
2019-11-01 09:45:17 +0100

    priority doesn't work

--- article/prefix.hs
+++ article/prefix.hs
@@ -24,17 +24,17 @@ accum n s = case digit s of
     [] -> [(n,s)]
     [(d,s')] -> accum (n * 10 + d) s'
 
-number :: Parser Char Token
-number (c:s) | isSpace c = number s
-number (c:s) | isDigit c = [(Num n,s')] where [(n,s')] = accum 0 (c:s)
-number _ = []
+num :: Parser Char Token
+num (c:s) | isSpace c = num s
+num (c:s) | isDigit c = [(Num n,s')] where [(n,s')] = accum 0 (c:s)
+num _ = []
 
-operator1 :: Char -> (Number -> Number) -> Parser Char Token
-operator1 o f (c:s) | c == o = [(Op1 c f, s)]
-operator1 _ _ _ = []
+parserForOp1 :: Char -> (Number -> Number) -> Parser Char Token
+parserForOp1 o f (c:s) | c == o = [(Op1 c f, s)]
+parserForOp1 _ _ _ = []
 
-negation = operator1 '~' negate
-factorial = operator1 '!' (\n -> product [1..n])
+negation = parserForOp1 '~' negate
+factorial = parserForOp1 '!' (\n -> product [1..n])
 
 infix 2 <|>
  
@@ -45,28 +45,34 @@ parserA <|> parserB = \s -> case parserA s of
 
 unary = negation <|> factorial
 
-operator2 :: Char -> (Number -> Number -> Number) -> Parser Char Token
-operator2 o f (c:s) | c == o = [(Op2 c f, s)]
-operator2 _ _ _ = []
+parserForOp2 :: Char -> (Number -> Number -> Number) -> Parser Char Token
+parserForOp2 o f (c:s) | c == o = [(Op2 c f, s)]
+parserForOp2 _ _ _ = []
 
-binary = foldl1 (<|>) (map (uncurry operator2) binOps)
+binary = foldl1 (<|>) (map (uncurry parserForOp2) binOps)
 
 binOps = [('+',(+)),('-',flip (-)),('*',(*)),('/',(flip div)),('%',flip mod)]
 
-infix 3 <&>
+token :: Parser a b -> Parser a [b]
+token parser = \s -> case parser s of
+    [(a,s')] -> [([a],s')]
+    [] -> []
 
-(<&>) :: Parser a b -> Parser a b -> Parser a b
-parserA <&> parserB = \s -> case parserA s of
+seqp :: Parser a [b] -> Parser a [b] -> Parser a [b]
+parserA `seqp` parserB = \s -> case parserA s of
     [] -> []
-    [(a,s')] -> parserB s'
+    [(as,s')] -> case parserB s' of
+        [] -> []
+        [(bs,s'')] -> [(as++bs,s'')]
 
+altp :: Parser a [b] -> Parser a [b] -> Parser a [b]
+parserA `altp` parserB =  \s -> case parserA s of
+    [(as,s')] -> [(as,s')]  
+    [] -> case parserB s of
+        [(bs,s')] -> [(bs,s')]
+        [] -> []
 
-num :: [Token] -> Parser Char [Token]
-num ts s = case number s of
-    [] -> [(ts,s)]
-    [(t,s')] -> [(ts ++ [t], s')]
-
-unaryOp :: [Token] -> Parser Char [Token] 
-unaryOp ts s = case unary s of
-    [(t,s')] -> [(ts ++ [t], s')]
-    [] -> [(ts,s)] 
+expr :: Parser Char [Token]
+expr =  token num 
+  `altp` (token binary `seqp` expr `seqp` expr)
+  `altp` (token unary `seqp` expr)
